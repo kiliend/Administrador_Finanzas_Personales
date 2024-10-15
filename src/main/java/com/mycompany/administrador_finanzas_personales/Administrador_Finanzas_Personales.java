cdd3704eb68a6d06916d4341e7f50a1d892ab231
kiliend <luis.condori.h98@gmail.com>
2024-10-14 22:02:15 -0500

    creaccion de proyecto

---
+++ src/main/java/com/mycompany/administrador_finanzas_personales/Administrador_Finanzas_Personales.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.administrador_finanzas_personales;
+
+/**
+ *
+ * @author Luan Condori
+ */
+public class Administrador_Finanzas_Personales {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
