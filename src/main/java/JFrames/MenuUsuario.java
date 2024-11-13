20d03cbcd654ea7525070512799a73be5eac9b16
KendrickB-S <kendrik.23.bs@gmail.com>
2024-11-12 21:26:53 -0500

    funcionalidad al menu usuario

--- src/main/java/JFrames/MenuUsuario.java
+++ src/main/java/JFrames/MenuUsuario.java
@@ -4,12 +4,17 @@
  */
 package JFrames;
 
+import Clases.RegistroUsuario;
+import Clases.Usuario;
+import Clases.UsuarioSesion;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author Luan Condori
  */
 public class MenuUsuario extends javax.swing.JFrame {
-
+    
     /**
      * Creates new form MenuUsuario
      */
@@ -30,18 +35,19 @@ public class MenuUsuario extends javax.swing.JFrame {
         jLabel2 = new javax.swing.JLabel();
         jLabel1 = new javax.swing.JLabel();
         jLabel3 = new javax.swing.JLabel();
-        etiquetaNombre = new javax.swing.JLabel();
         jLabel5 = new javax.swing.JLabel();
-        etiqueraEmail = new javax.swing.JLabel();
         jLabel7 = new javax.swing.JLabel();
         etiquetaNCuenta = new javax.swing.JLabel();
         btnEditarDatos = new javax.swing.JButton();
         jLabel9 = new javax.swing.JLabel();
-        etiquetaUsuario = new javax.swing.JLabel();
         jLabel11 = new javax.swing.JLabel();
-        etiquetaTelefono = new javax.swing.JLabel();
         jLabel13 = new javax.swing.JLabel();
         btnRegistrarCuenta = new javax.swing.JButton();
+        txNombreEditar = new javax.swing.JTextField();
+        txEmailEditar = new javax.swing.JTextField();
+        txDNIEditar = new javax.swing.JTextField();
+        txTelefonoEditar = new javax.swing.JTextField();
+        jButton1 = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -55,14 +61,8 @@ public class MenuUsuario extends javax.swing.JFrame {
         jLabel3.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
         jLabel3.setText("Nombre :");
 
-        etiquetaNombre.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
-        etiquetaNombre.setText("Nombre Usuario");
-
         jLabel5.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
         jLabel5.setText("Email :");
-
-        etiqueraEmail.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
-        etiqueraEmail.setText("Email Usuario");
 
         jLabel7.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
         jLabel7.setText("Cuenta  Bancaria:");
@@ -83,14 +83,8 @@ public class MenuUsuario extends javax.swing.JFrame {
         jLabel9.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
         jLabel9.setText("DNI:");
 
-        etiquetaUsuario.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
-        etiquetaUsuario.setText("DNI Usuario");
-
         jLabel11.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
         jLabel11.setText("Telefono :");
-
-        etiquetaTelefono.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
-        etiquetaTelefono.setText("Telefono Usuario");
 
         jLabel13.setText("----------------------------------------------------------------");
 
@@ -101,6 +95,14 @@ public class MenuUsuario extends javax.swing.JFrame {
         btnRegistrarCuenta.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 btnRegistrarCuentaActionPerformed(evt);
+            }
+        });
+
+        jButton1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Regresar Registros.png"))); // NOI18N
+        jButton1.setText("Cancelar");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
             }
         });
 
@@ -120,17 +122,15 @@ public class MenuUsuario extends javax.swing.JFrame {
                             .addComponent(jLabel11))
                         .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addGroup(jPanel1Layout.createSequentialGroup()
-                                .addGap(54, 54, 54)
-                                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                                    .addComponent(etiquetaUsuario)
-                                    .addComponent(etiquetaTelefono)
-                                    .addComponent(etiquetaNombre)
-                                    .addComponent(etiqueraEmail)))
-                            .addGroup(jPanel1Layout.createSequentialGroup()
                                 .addGap(43, 43, 43)
-                                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                                    .addComponent(btnEditarDatos)
-                                    .addComponent(jLabel1)))))
+                                .addComponent(jLabel1))
+                            .addGroup(jPanel1Layout.createSequentialGroup()
+                                .addGap(18, 18, 18)
+                                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                                    .addComponent(txEmailEditar, javax.swing.GroupLayout.DEFAULT_SIZE, 162, Short.MAX_VALUE)
+                                    .addComponent(txNombreEditar)
+                                    .addComponent(txDNIEditar)
+                                    .addComponent(txTelefonoEditar)))))
                     .addGroup(jPanel1Layout.createSequentialGroup()
                         .addGap(13, 13, 13)
                         .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -138,7 +138,11 @@ public class MenuUsuario extends javax.swing.JFrame {
                             .addGroup(jPanel1Layout.createSequentialGroup()
                                 .addComponent(jLabel7, javax.swing.GroupLayout.PREFERRED_SIZE, 135, javax.swing.GroupLayout.PREFERRED_SIZE)
                                 .addGap(32, 32, 32)
-                                .addComponent(etiquetaNCuenta)))))
+                                .addComponent(etiquetaNCuenta))
+                            .addGroup(jPanel1Layout.createSequentialGroup()
+                                .addComponent(btnEditarDatos)
+                                .addGap(68, 68, 68)
+                                .addComponent(jButton1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))))
                 .addContainerGap(44, Short.MAX_VALUE))
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
@@ -152,27 +156,29 @@ public class MenuUsuario extends javax.swing.JFrame {
                     .addGroup(jPanel1Layout.createSequentialGroup()
                         .addGap(17, 17, 17)
                         .addComponent(jLabel2)
-                        .addGap(34, 34, 34)
+                        .addGap(33, 33, 33)
                         .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                             .addComponent(jLabel3)
-                            .addComponent(etiquetaNombre))
+                            .addComponent(txNombreEditar, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                         .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                             .addComponent(jLabel5)
-                            .addComponent(etiqueraEmail)))
+                            .addComponent(txEmailEditar, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
                     .addGroup(jPanel1Layout.createSequentialGroup()
                         .addGap(36, 36, 36)
                         .addComponent(jLabel1)))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel9)
-                    .addComponent(etiquetaUsuario))
+                    .addComponent(txDNIEditar, javax.swing.GroupLayout.PREFERRED_SIZE, 24, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel11)
-                    .addComponent(etiquetaTelefono))
+                    .addComponent(txTelefonoEditar, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(18, 18, 18)
-                .addComponent(btnEditarDatos, javax.swing.GroupLayout.PREFERRED_SIZE, 36, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(btnEditarDatos, javax.swing.GroupLayout.PREFERRED_SIZE, 36, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jButton1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addComponent(jLabel13)
                 .addGap(18, 18, 18)
@@ -203,12 +209,42 @@ public class MenuUsuario extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void btnEditarDatosActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnEditarDatosActionPerformed
-        // TODO add your handling code here:
+        // obtener los datos de los campos de texto
+        String nombre = txNombreEditar.getText();
+        String email = txEmailEditar.getText();
+        String dni = txDNIEditar.getText();
+        String telefono = txTelefonoEditar.getText();
+
+        //obtener el id del usuario que inicio sesion
+        int idUsuario = UsuarioSesion.getUserId();
+        //comprobar que el id de usuario haya iniciado sesion
+        if (idUsuario == 0) {
+            JOptionPane.showMessageDialog(this, "Error: No se ha iniciado sesión.");
+            return;
+        }
+
+        // Crear una instancia de Usuario
+        Usuario usuario = new Usuario(nombre, "", dni, telefono, email, "", "");
+        usuario.setIdUsuario(idUsuario);
+
+        RegistroUsuario registroUsuario = new RegistroUsuario();
+        boolean actualizado = registroUsuario.editarUsuario(usuario);
+
+        // Verifica si la actualización fue exitosa
+        if (actualizado) {
+            JOptionPane.showMessageDialog(this, "Datos actualizados correctamente.");
+        } else {
+            JOptionPane.showMessageDialog(this, "Error al actualizar los datos.");
+        }
     }//GEN-LAST:event_btnEditarDatosActionPerformed
 
     private void btnRegistrarCuentaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnRegistrarCuentaActionPerformed
         // TODO add your handling code here:
     }//GEN-LAST:event_btnRegistrarCuentaActionPerformed
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        this.dispose();
+    }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -248,11 +284,8 @@ public class MenuUsuario extends javax.swing.JFrame {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton btnEditarDatos;
     private javax.swing.JButton btnRegistrarCuenta;
-    private javax.swing.JLabel etiqueraEmail;
     private javax.swing.JLabel etiquetaNCuenta;
-    private javax.swing.JLabel etiquetaNombre;
-    private javax.swing.JLabel etiquetaTelefono;
-    private javax.swing.JLabel etiquetaUsuario;
+    private javax.swing.JButton jButton1;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel11;
     private javax.swing.JLabel jLabel13;
@@ -262,5 +295,9 @@ public class MenuUsuario extends javax.swing.JFrame {
     private javax.swing.JLabel jLabel7;
     private javax.swing.JLabel jLabel9;
     private javax.swing.JPanel jPanel1;
+    private javax.swing.JTextField txDNIEditar;
+    private javax.swing.JTextField txEmailEditar;
+    private javax.swing.JTextField txNombreEditar;
+    private javax.swing.JTextField txTelefonoEditar;
     // End of variables declaration//GEN-END:variables
 }
